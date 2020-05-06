dcddfe52f09eb2d8fb54a8e8984b82a6520b5298
Nina Reynolds <nina@girlhours.info>
2020-05-05 23:34:30 -0500

    feat: humanize js-quantities units

--- src/components/AnswerField.js
+++ src/components/AnswerField.js
@@ -3,37 +3,39 @@ import React from 'react';
 import { Button, Paper, TextField, Card, InputAdornment } from '@material-ui/core';
 // const anime = require('animejs');
 import anime from 'animejs/lib/anime.es.js';
-import Qty from 'js-quantities';
-const quantities = require('js-quantities');
+// import Qty from 'js-quantities';
+const Qty = require('js-quantities');
 
 const fToC = 5;
 const fToK = 5;
-const xtox = function (fromUnit, toUnit, value) {
-    //TODO: i know there's a more efficient way of doing this...
-    // const values = {
-    //     fahrenheitcelsius: function (value) { 
-    //         return 3 + 3;
-    //     },
-    //     celsiusfahrenheit: cToF,
-
-    //     fahrenheitkelvin: f,
-    //     kelvinfahrenheit: f,
-
-    //     celsiuskelvin,
-    //     kelvincelsius,
-
-    // } 
+const checkAnswer = function (studentAnswer, correctAnswer) {
     try {
-    const quantity = new Qty(value + ` ${fromUnit}`);
-    return Qty.to(toUnit);
-    
-
-    } catch(e) { 
-        return e
+        return Qty(studentAnswer).eq(Qty(correctAnswer));
+    } catch (e) {
+        return 0;
     }
+}
+const humanize = function (value) {
+    let output = value;
+    output = (value == "fahrenheit") ? "tempF" : value;
+    output = (value == "celsius") ? "tempC" : value;
+    output = (value == "kelvin") ? "tempK" : value;
+    return value;
 
-    // return entry;
-
+}
+const round = function (decimals) {
+    return function (scalar, units) {
+        var pow = Math.pow(10, decimals);
+        return `${scalar.toFixed(2)} ${units}`
+    }
+}
+const xtox = function (fromUnit, toUnit, value) {
+    try {
+        const quantity = new Qty(`${value} ${humanize(fromUnit)}`);
+        return quantity.to(humanize(toUnit)).format(round(2));
+    } catch (e) {
+        return "invalid"
+    }
 }
 
 class ResultField extends React.Component {
@@ -49,6 +51,7 @@ class AnswerField extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            'correctAnswer': ''
         };
         this.handleChange = this.handleChange.bind(this);
         this.answerAnimator = anime({
@@ -65,25 +68,24 @@ class AnswerField extends React.Component {
             ...this.state,
             [event.target.name]: event.target.value,
         },
-            this.setState({
-                ...this.state,
-                correctAnswer: xtox(this.state.inputUnit, this.state.targetUnit, this.state.inputValue
+            function () {
+                this.setState({
+                    ...this.state,
+                    correctAnswer: xtox(this.state.inputUnit, this.state.targetUnit, this.state.inputValue),
+                    answerStatus: checkAnswer(this.state.studentResponse, xtox(this.state.inputUnit, this.state.targetUnit, this.state.inputValue))
+                })
+                console.log(`answer: ${xtox(this.state.inputUnit, this.state.targetUnit, this.state.inputValue)}`)
 
-                )
-            })
+            }
         );
         console.log(event.target.name)
         console.log(event.target.value)
 
-        // this.answerAnimator.restart();
-        // this.answerAnimator.play();
         anime({
             targets: document.querySelector("#studentResponse"),
             rotate: 360
         });
 
-        // xtox("fahrenheit","celsius",3)
-        // result = xtox()
     }
 
     render() {
@@ -91,12 +93,11 @@ class AnswerField extends React.Component {
             <TextField value={this.inputValue} onChange={this.handleChange} variant="outlined" name="inputValue" helperText="Numerical Value"></TextField>
             <TextField onChange={this.handleChange} variant="outlined" name="inputUnit" helperText="Unit of Measurement"></TextField>
             <TextField onChange={this.handleChange} variant="outlined" name="targetUnit" helperText="Unit to Convert To"></TextField>
-            <TextField onChange={this.handleChange} variant="outlined" id="studentResponse" name="studentResponse" helperText="Student Response"></TextField>
+            <TextField error={!this.state.answerStatus} onChange={this.handleChange} variant="outlined" id="studentResponse" name="studentResponse" helperText="Student Response"></TextField>
 
-            {/* <TextField multiline onChange={this.handleChange} > */}
+            <TextField variant="outlined" helperText="Correct Answer" value={this.state.correctAnswer} />
 
             {/* <InputAdornment></InputAdornment></TextField> */}
-            <div>{this.props.value}</div>
         </div>
     }
 
