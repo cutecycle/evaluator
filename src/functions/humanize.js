540f565b2870d36586c8d18aa4ef24e0ec770fc9
Nina Reynolds <nina@girlhours.info>
2020-05-09 16:39:30 -0500

    feat: intuitive cubic units

--- src/functions/humanize.js
+++ src/functions/humanize.js
@@ -4,7 +4,27 @@ const humanize = function (value) {
     output = (value === "celsius") ? "tempC" : output;
     output = (value === "kelvin") ? "tempK" : output;
     output = (value === "rankine") ? "tempR" : output;
+    output = powerify(output);
+
     return output;
+
+}
+
+const powerify = function (value) {
+    try {
+        let powers = {
+            "cubic-": "3",
+            "square-": "2"
+        }
+        let unit = value.replace(/[a-zA-Z]*-/, "");
+        let power = powers[value.match(/[a-zA-Z]*-/)];
+        if (typeof power === 'undefined') {
+            return unit;
+        }
+        return `${unit}^${power}`
+    } catch (e) {
+        throw "error converting exponential unit"
+    }
 
 }
 module.exports = { humanize }
